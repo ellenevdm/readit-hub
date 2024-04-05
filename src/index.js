d82c5b4949cf5e1e41bd36ec22ce13505b801a47
Ellene <ellene.vdmeijden@gmail.com>
2024-04-05 09:51:21 +0200

    sadiasduidhfoiasf

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import "./index.css";
-// Bootstrap CSS
 import "../node_modules/react-bootstrap/dist/react-bootstrap";
-// Bootstrap Bundle JS
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import App from "./App/App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Provider} from "react-redux";
-import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./store/store";
+import store from "./Store/store";
+
+// Bootstrap CSS
+
+// Bootstrap Bundle JS
 
 //React Router Imports:
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
