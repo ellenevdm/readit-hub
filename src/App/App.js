25a68fb85bda9256cc3363068edd1a16d9c78d51
Ellene <ellene.vdmeijden@gmail.com>
2024-04-05 11:00:28 +0200

    removed default path

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,27 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import DetailedPostView from "../Features/posts/DetailedPostView";
-import HomePage from "../Pages/HomePage";
 import Header from "../Components/Header";
+import HomePage from "../Pages/HomePage";
 
 // import Home from './Features/Home/Home';
 
 const App = () => {
-    return (
-        <Router>
-            <Header/>
-            <Routes>
-                <Route
-                    path="/reddit-project"
-                    element={<HomePage/>}
-                />
-                <Route
-                    path="/posts/:postId"
-                    element={<DetailedPostView/>}
-                />
-            </Routes>
-        </Router>
-    );
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={<HomePage />}
+				/>
+				<Route
+					path="/posts/:postId"
+					element={<DetailedPostView />}
+				/>
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
