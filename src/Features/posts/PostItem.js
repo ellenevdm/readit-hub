bf99e306b76173b2f3cfc6f86a063a382d8bb51b
ellene <ellenevdmeijden@gmail.com>
2024-09-26 14:51:48 +0200

    added clearPost details reducer

--- src/Features/posts/PostItem.js
+++ src/Features/posts/PostItem.js
@@ -1,127 +1,132 @@
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 
-import {Button, Col, Row, Stack} from "react-bootstrap";
+import { Button, Col, Row, Stack } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
-import {LinkContainer} from "react-router-bootstrap";
-import {FaRegCommentAlt} from "react-icons/fa";
-import React, {useEffect, useRef, useState} from "react";
-import {formatScoreNumber} from "../../Utilities/formatNumber";
-import {ImArrowDown, ImArrowUp} from "react-icons/im";
-import {useNavigate} from "react-router";
+import { LinkContainer } from "react-router-bootstrap";
+import { FaRegCommentAlt } from "react-icons/fa";
+import React, { useEffect, useRef, useState } from "react";
+import { formatScoreNumber } from "../../Utilities/formatNumber";
+import { ImArrowDown, ImArrowUp } from "react-icons/im";
+import { useNavigate } from "react-router";
+import { clearPostDetails } from "../../Store/redditSlice";
 
-function PostItem({post}) {
-    const dispatch = useDispatch();
-    const [showButton, setShowButton] = useState(false)
-    const containerRef = useRef(null)
-    const navigate = useNavigate()
+function PostItem({ post }) {
+  const dispatch = useDispatch();
+  const [showButton, setShowButton] = useState(false);
+  const containerRef = useRef(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const handleOverflow = () => {
-            const container = containerRef.current
-            if (container) {
-                setShowButton(container.scrollHeight > container.offsetHeight)
-            }
-        };
+  useEffect(() => {
+    const handleOverflow = () => {
+      const container = containerRef.current;
+      if (container) {
+        setShowButton(container.scrollHeight > container.offsetHeight);
+      }
+    };
 
-        window.addEventListener('resize', handleOverflow);
-        handleOverflow();
+    window.addEventListener("resize", handleOverflow);
+    handleOverflow();
 
-        return () => {
-            window.removeEventListener('resize', handleOverflow)
-        }
-    }, [])
+    return () => {
+      window.removeEventListener("resize", handleOverflow);
+    };
+  }, []);
 
+  function openPage(s) {
+    dispatch(clearPostDetails());
+    navigate(s);
+  }
 
-    function openPage(s) {
-        navigate(s)
-    }
+  return (
+    <Card
+      className="m-2"
+      style={{}}
+      onClick={() => openPage(`/posts/${post.id}`)}
+    >
+      <Row style={{ marginLeft: 0 }}>
+        <Col
+          style={{
+            borderRight: "1px solid #ccc",
+            textAlign: "center",
+            maxWidth: "min-content",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            className="d-flex align-items-center"
+            style={{ marginTop: "5px" }}
+          >
+            <Stack gap={0}>
+              <div>
+                <ImArrowUp />
+              </div>
+              <div>{formatScoreNumber(post.score)}</div>
+              <div>
+                <ImArrowDown />
+              </div>
+            </Stack>
+          </div>
+        </Col>
+        <Col style={{ marginTop: "0px", flex: "1" }}>
+          <Card.Header>
+            <Card.Subtitle>{post.subreddit_name_prefixed}</Card.Subtitle>
+          </Card.Header>
 
-    return (
-        <Card className="m-2" style={{}} onClick={() => openPage(`/posts/${post.id}`)}>
+          <Card.Body
+            ref={containerRef}
+            style={{
+              maxHeight: "30rem",
+              overflow: "hidden",
+              position: "relative",
+            }}
+          >
+            <Card.Title>
+              <h4>{post.title}</h4>
+            </Card.Title>
 
-            <Row style={{marginLeft: 0}}>
-                <Col style={{
-                    borderRight: "1px solid #ccc",
-                    textAlign: "center",
-                    maxWidth: "min-content",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                    <div className="d-flex align-items-center" style={{marginTop: "5px"}}>
-                        <Stack gap={0}>
-                            <div>
-                                <ImArrowUp/>
-                            </div>
-                            <div>{formatScoreNumber(post.score)}</div>
-                            <div>
-                                <ImArrowDown/>
-                            </div>
-                        </Stack>
-                    </div>
-                </Col>
-                <Col style={{marginTop: "0px", flex: "1"}}>
-                    <Card.Header>
-                        <Card.Subtitle>{post.subreddit_name_prefixed}</Card.Subtitle>
-                    </Card.Header>
+            {/* <em>{post.author}</em> */}
+            {post.preview && (
+              <Card.Img
+                className="mr-3"
+                src={post.preview.images[0].source.url}
+              />
+            )}
 
-                    <Card.Body ref={containerRef}
-                               style={{maxHeight: '30rem', overflow: 'hidden', position: 'relative'}}>
+            {showButton && (
+              <div
+                className="fade-effect"
+                style={{
+                  position: "absolute",
+                  top: "75%",
+                  left: 0,
+                  width: "100%",
+                  height: "25%", // Adjust as needed
+                  background: "linear-gradient(rgba(255, 255, 255, 0), white)",
+                  display: "flex",
+                  alignItems: "end",
+                  justifyContent: "center",
+                }}
+              >
+                <LinkContainer to={`/posts/${post.id}`}>
+                  <Button variant="primary" type="button" className=" p-2">
+                    View more ...
+                  </Button>
+                </LinkContainer>
+              </div>
+            )}
+          </Card.Body>
 
-                        <Card.Title><h4>{post.title}</h4></Card.Title>
-
-                        {/* <em>{post.author}</em> */}
-                        {post.preview && (
-                            <Card.Img
-                                className="mr-3"
-                                src={post.preview.images[0].source.url}
-                            />
-                        )}
-
-                        {showButton && (
-                            <div className='fade-effect'
-                                 style={{
-                                     position: 'absolute',
-                                     top: '75%',
-                                     left: 0,
-                                     width: '100%',
-                                     height: '25%', // Adjust as needed
-                                     background: 'linear-gradient(rgba(255, 255, 255, 0), white)',
-                                     display: 'flex',
-                                     alignItems: 'end',
-                                     justifyContent: 'center'
-                                 }}
-                            >
-
-                                <LinkContainer to={`/posts/${post.id}`}>
-                                    <Button
-                                        variant="primary"
-                                        type="button"
-                                        className=" p-2"
-                                    >
-                                        View more ...
-                                    </Button>
-                                </LinkContainer>
-
-                            </div>
-                        )}
-
-
-                    </Card.Body>
-
-
-                    <Card.Footer>
-                        <Stack direction="horizontal" gap={3}>
-
-
-                            <FaRegCommentAlt/> {post.num_comments} Comments
-                            
-                        </Stack>
-                    </Card.Footer>
-                </Col>
-            </Row>
-        </Card>
-    );
+          <Card.Footer>
+            <Stack direction="horizontal" gap={3}>
+              <FaRegCommentAlt /> {post.num_comments} Comments
+            </Stack>
+          </Card.Footer>
+        </Col>
+      </Row>
+    </Card>
+  );
 }
 
 export default PostItem;
--- src/Store/redditSlice.js
+++ src/Store/redditSlice.js
@@ -1,166 +1,185 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import reddit from "../Api/redditApi";
 
 export const fetchPosts = createAsyncThunk(
-    "reddit/fetchPosts",
-    async ({subreddit, searchQuery}) => {
-        try {
-            let posts;
-            if (searchQuery) {
-                posts = await reddit.search({
-                    query: searchQuery,
-                    sort: "relevance",
-                    time: "all",
-                    limit: 10,
-                });
-            } else {
-                posts = await reddit.getHot(subreddit, {limit: 10});
-            }
+  "reddit/fetchPosts",
+  async ({ subreddit, searchQuery }) => {
+    try {
+      let posts;
+      if (searchQuery) {
+        posts = await reddit.search({
+          query: searchQuery,
+          sort: "relevance",
+          time: "all",
+          limit: 10,
+        });
+      } else {
+        posts = await reddit.getHot(subreddit, { limit: 10 });
+      }
 
-
-            return posts;
-        } catch (error) {
-            console.log(error);
-        }
+      return posts;
+    } catch (error) {
+      console.log(error);
     }
+  }
 );
 
 export const fetchPostDetails = createAsyncThunk(
-    "reddit/fetchPostDetails",
-    async (postId) => {
-        try {
-            debugger;
-            const post = await reddit.getSubmission(postId).fetch();
+  "reddit/fetchPostDetails",
+  async (postId, { dispatch }) => {
+    dispatch(clearPostDetails());
+    try {
+      const post = await reddit.getSubmission(postId).fetch();
 
-            const allComments = post.comments;
+      const allComments = post.comments;
 
-            // Extract only the serializable information from the post
-            let serializablePost = {
-                id: post.id,
-                title: post.title,
-                author: post.author.name,
-                subreddit_name_prefixed: post.subreddit_name_prefixed,
-                selftext: post.selftext,
-                selftext_html: post.selftext_html,
-                score: post.score,
-                num_comments: post.num_comments
+      // Extract only the serializable information from the post
+      let serializablePost = {
+        id: post.id,
+        title: post.title,
+        author: post.author.name,
+        subreddit_name_prefixed: post.subreddit_name_prefixed,
+        selftext: post.selftext,
+        selftext_html: post.selftext_html,
+        score: post.score,
+        num_comments: post.num_comments,
+      };
 
-            };
+      if (post.media && post.media.reddit_video) {
+        serializablePost.media = {
+          reddit_video: post.media.reddit_video.fallback_url,
+        };
+      } else if (!post.media && post.preview && post.preview.images) {
+        serializablePost.preview = {
+          images: post.preview.images.map((image) => image.source.url),
+        };
+      }
 
-            if (post.media && post.media.reddit_video) {
-                serializablePost.media = {
-                    reddit_video: post.media.reddit_video.fallback_url
-                }
-            } else if (!post.media && post.preview && post.preview.images) {
-                serializablePost.preview = {
-                    images: post.preview.images.map((image) => image.source.url),
-                };
-            }
+      // Take the first 10 comments
+      let slicedComments = allComments.slice(0, 10);
 
-            // Take the first 10 comments
-            let slicedComments = allComments.slice(0, 10);
+      const simplifiedComments = [];
+      for (const comment of slicedComments) {
+        // const commentDetails = await reddit.getComment(comment.id).fetch();
+        simplifiedComments.push({
+          id: comment.id,
+          body: comment.body,
+          body_html: comment.body_html,
+          score: comment.score,
+          author: comment.author.name,
+        });
+      }
 
-
-            const simplifiedComments = [];
-            for (const comment of slicedComments) {
-                debugger;
-                // const commentDetails = await reddit.getComment(comment.id).fetch();
-                simplifiedComments.push({
-                    id: comment.id,
-                    body: comment.body,
-                    body_html: comment.body_html,
-                    score: comment.score,
-                    author: comment.author.name
-                });
-
-            }
-
-
-            return {post: serializablePost, comments: simplifiedComments};
-        } catch (error) {
-            console.log(error);
-
-        }
+      return { post: serializablePost, comments: simplifiedComments };
+    } catch (error) {
+      console.log(error);
     }
+  }
 );
 
 const redditSlice = createSlice({
-    name: "redditPosts",
-    initialState: {
-        selectedSubreddit: "",
-        searchQuery: "",
-        selectedPostId: null,
-        posts: [],
-        postDetails: {
-            post: {
-                id: "",
-                title: "",
-                author: "",
-                subreddit_name_prefixed: "",
-                selftext: "",
-                selftext_html: "",
-                num_comments: 0,
-                media: {
-                    reddit_video: ""
-                },
-                score: 0,
-                preview: {
-                    images: [],
-                },
-            },
-            comments: [],
+  name: "redditPosts",
+  initialState: {
+    selectedSubreddit: "",
+    searchQuery: "",
+    selectedPostId: null,
+    posts: [],
+    postDetails: {
+      post: {
+        id: "",
+        title: "",
+        author: "",
+        subreddit_name_prefixed: "",
+        selftext: "",
+        selftext_html: "",
+        num_comments: 0,
+        media: {
+          reddit_video: "",
         },
-        status: "idle",
-        error: null,
-    },
-    reducers: {
-        setSelectedSubreddit: (state, {payload}) => {
-            state.selectedSubreddit = payload;
+        score: 0,
+        preview: {
+          images: [],
         },
-        setSearchQuery: (state, {payload}) => {
-            state.searchQuery = payload;
-        }
-        ,
+      },
+      comments: [],
     },
-    extraReducers: (builder) =>
-        builder
-            .addCase(fetchPosts.pending, (state) => {
-                state.status = "loading";
-            })
-            .addCase(fetchPosts.fulfilled, (state, {payload}) => {
-                state.status = "success";
-                state.posts = payload;
-            })
-            .addCase(fetchPosts.rejected, (state, {payload}) => {
-                state.error = payload;
-                state.status = "rejected";
-            })
-            .addCase(fetchPostDetails.pending, (state) => {
-                state.status = "loading";
-                console.log("Loading...");
-            })
-            .addCase(fetchPostDetails.fulfilled, (state, {payload}) => {
-                state.status = "success";
-                state.postDetails.post = payload.post;
-                state.postDetails.comments = payload.comments;
-            })
-            .addCase(fetchPostDetails.rejected, (state, {payload}) => {
-                state.error = payload;
-                state.status = "rejected";
-            }),
+    status: "idle",
+    error: null,
+  },
+  reducers: {
+    setSelectedSubreddit: (state, { payload }) => {
+      state.selectedSubreddit = payload;
+    },
+    setSearchQuery: (state, { payload }) => {
+      state.searchQuery = payload;
+    },
+    clearPostDetails: (state) => {
+      state.postDetails = {
+        post: {
+          id: "",
+          title: "",
+          author: "",
+          subreddit_name_prefixed: "",
+          selftext: "",
+          selftext_html: "",
+          num_comments: 0,
+          media: {
+            reddit_video: "",
+          },
+          score: 0,
+          preview: {
+            images: [],
+          },
+        },
+        comments: [],
+      };
+    },
+  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = "loading";
+        console.log("Fetching posts...");
+      })
+      .addCase(fetchPosts.fulfilled, (state, { payload }) => {
+        state.status = "success";
+        state.posts = payload;
+        console.log("Fetched posts successfully");
+      })
+      .addCase(fetchPosts.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.status = "rejected";
+        console.log("Error fetching posts");
+      })
+      .addCase(fetchPostDetails.pending, (state) => {
+        state.status = "loading";
+        console.log("Fetching post details...");
+      })
+      .addCase(fetchPostDetails.fulfilled, (state, { payload }) => {
+        state.status = "success";
+        state.postDetails.post = payload.post;
+        state.postDetails.comments = payload.comments;
+        console.log("Fetched post details and comments successfully");
+      })
+      .addCase(fetchPostDetails.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.status = "rejected";
+        console.log("Error fetching post comments and details");
+      }),
 });
 
 export default redditSlice.reducer;
 export const {
-    setSearchQuery,
-    setSelectedSubreddit,
+  setSearchQuery,
+  setSelectedSubreddit,
 
-    setSelectedPostId,
+  setSelectedPostId,
+  clearPostDetails,
 } = redditSlice.actions;
 
 export const selectSelectedSubreddit = (state) =>
-    state.reddit.selectedSubreddit; //
+  state.reddit.selectedSubreddit; //
 export const selectPosts = (state) => state.reddit.posts; //
 export const selectDetailedPost = (state) => state.reddit.postDetails.post;
 export const selectComments = (state) => state.reddit.postDetails.comments; //
